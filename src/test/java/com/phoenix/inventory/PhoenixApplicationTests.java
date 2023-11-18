71dbcf2cdb3de780a502b8a1f8f34c9059e01999
KhaledSaiidi <Khaled.Saiidi@outlook.com>
2023-11-18 11:16:38 +0100

    First commit, creating the spring project

---
+++ src/test/java/com/phoenix/inventory/PhoenixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phoenix.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoenixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
