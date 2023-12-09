330e565a6753ca6de6ae7b51b846a5d0f7c0f377
KhaledSaiidi <Khaled.Saiidi@outlook.com>
2023-12-09 21:13:41 +0100

    launching java pro

---
+++ keycloakuser/src/test/java/com/phoenix/keycloakuser/KeycloakuserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phoenix.keycloakuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakuserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
