229b3ce65d7df0d1ac496897be90918a915b7944
KhaledSaiidi <Khaled.Saiidi@outlook.com>
2023-12-13 20:22:36 +0100

    adding charts.js

--- phoenixfront/src/app/component/dashboard/dashboard.component.ts
+++ phoenixfront/src/app/component/dashboard/dashboard.component.ts
@@ -1,10 +1,93 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import Chart from 'chart.js/auto';
+
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit{
+
+  ngOnInit() {
+   this.initializeChart();
+  }
+
+  initializeChart(): void {
+    var ctx: any = document.getElementById("chart-line");
+    var ctx1 = ctx.getContext("2d");
+    var gradientStroke1 = ctx1.createLinearGradient(0, 230, 0, 50);
+    
+    gradientStroke1.addColorStop(1, 'rgba(94, 114, 228, 0.2)');
+    gradientStroke1.addColorStop(0.2, 'rgba(94, 114, 228, 0.0)');
+    gradientStroke1.addColorStop(0, 'rgba(94, 114, 228, 0)');
+    new Chart(ctx1, {
+    type: "line",
+      data: {
+      labels: ["Jan", "Fev", "Mar", "Avr", "May", "Juin", "Jul", "Aout", "Sep", "Oct", "Nov", "Dec"],
+      datasets: [{
+        label: "Nombre de jours d'absence",
+        tension: 0.4,
+        pointRadius: 0,
+        borderColor: "#5e72e4",
+        backgroundColor: gradientStroke1,
+        borderWidth: 3,
+        fill: true,
+        data: [20, 30, 10, 20, 20, 15, 16, 20, 15, 14, 12, 10]
+    }],
+      },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+      plugins: {
+        legend: {
+        display: false,
+        }
+      },
+      interaction: {
+        intersect: false,
+        mode: 'index',
+        },
+    scales: {
+    y: {
+      grid: {
+        display: true,
+        drawOnChartArea: true,
+        drawTicks: false,
+        },
+      ticks: {
+        display: true,
+        padding: 10,
+        color: '#fbfbfb',
+      font: {
+        size: 11,
+        family: "Open Sans",
+        style: 'normal',
+        lineHeight: 2
+        },
+      }
+    },
+      x: {
+        grid: {
+          display: false,
+          drawOnChartArea: false,
+          drawTicks: false,
+          },
+        ticks: {
+          display: true,
+          color: '#ccc',
+          padding: 20,
+        font: {
+          size: 11,
+          family: "Open Sans",
+          style: 'normal',
+          lineHeight: 2
+              },
+            }
+          },
+       },
+      },
+    });
+  }
 
 }
