b08c0f00e871b42af3bc0fb3a40e275a26060429
KhaledSaiidi <Khaled.Saiidi@outlook.com>
2024-01-16 18:02:51 +0100

    starting micro 2

---
+++ phoenix/stock/src/main/java/com/phoenix/Stock.java
@@ -0,0 +1,11 @@
+package com.phoenix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Stock {
+    public static void main(String[] args) {
+        SpringApplication.run(Stock.class, args);
+    }
+}
