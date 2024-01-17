4467721bdf63df052896b71010ae00fc5e36deff
KhaledSaiidi <Khaled.Saiidi@outlook.com>
2024-01-17 04:28:40 +0100

    config server & CORS Policy

--- phoenix/gateway/src/main/java/com/phoenix/config/SecurityConfig.java
+++ phoenix/gateway/src/main/java/com/phoenix/config/SecurityConfig.java
@@ -2,10 +2,18 @@ package com.phoenix.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -23,4 +31,29 @@ public class SecurityConfig {
                 .oauth2ResourceServer(spec -> spec.jwt(Customizer.withDefaults()));
         return serverHttpSecurity.build();
     }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        return exchange -> {
+            ServerHttpResponse response = exchange.getResponse();
+            HttpHeaders headers = response.getHeaders();
+
+            headers.setAccessControlAllowOrigin("*");
+            headers.setAccessControlAllowCredentials(true);
+            headers.setAccessControlAllowHeaders(Arrays.asList("Authorization", "Content-Type"));
+
+            if (exchange.getRequest().getMethod() == HttpMethod.OPTIONS) {
+                headers.setAccessControlAllowMethods(Arrays.asList(
+                        HttpMethod.GET,
+                        HttpMethod.POST,
+                        HttpMethod.PUT,
+                        HttpMethod.DELETE
+                ));
+                headers.setAccessControlMaxAge(3600L);
+                return new CorsConfiguration().applyPermitDefaultValues();
+            }
+            return null;
+        };
+    }
+
 }
--- phoenix/keycloakuser/src/main/java/com/phoenix/controller/Controller.java
+++ phoenix/keycloakuser/src/main/java/com/phoenix/controller/Controller.java
@@ -26,7 +26,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@CrossOrigin("*")
 public class Controller {
     @Autowired
     KeycloakSecurityUtil keycloakUtil;
