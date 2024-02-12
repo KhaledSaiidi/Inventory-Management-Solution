3ffa83168559118d3223ce00ce6cd66f2261c9e7
KhaledSaiidi <Khaled.Saiidi@outlook.com>
2024-02-12 20:52:16 +0100

    config Renews (last min)/ Renews threshold

--- phoenix/keycloakuser/src/main/java/com/phoenix/KeycloakUser.java
+++ phoenix/keycloakuser/src/main/java/com/phoenix/KeycloakUser.java
@@ -2,8 +2,10 @@ package com.phoenix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class KeycloakUser {
     public static void main(String[] args) {
         SpringApplication.run(KeycloakUser.class, args);
--- phoenix/stock/src/main/java/com/phoenix/Stock.java
+++ phoenix/stock/src/main/java/com/phoenix/Stock.java
@@ -2,10 +2,12 @@ package com.phoenix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Stock {
     public static void main(String[] args) {
         SpringApplication.run(Stock.class, args);
